04bad2d2880c54e0f158f67eaf4d187a2d5dfbd4
mazenabousaidaub <125895731+mazenabousaidaub@users.noreply.github.com>
2023-02-20 16:39:41 +0200

    Add files via upload

---
+++ Cleint_mazenabousaid.py
@@ -0,0 +1,22 @@
+from socket import *
+import datetime
+import uuid
+host ='127.0.0.1' #str(gethostbyname(gethostname())) works on private networks as Firewall Prevents Port Access by default on public networks
+# we can use the localhost IP since both server and client run on same machine
+port = 8696  # port number of the proxy server
+client_socket = socket(AF_INET, SOCK_STREAM)  # instantiate socket to connect to proxy server
+try:
+    client_socket.connect((host, port))  # connect to the proxy server
+    ipaddress = input("Input IP address of destination server: ")  # input ip to send to server
+    client_socket.send(ipaddress.encode()) #sends to proxy
+    request_timestamp = datetime.datetime.now()
+    print("Request sent to server to connect to " + str(ipaddress) + " at " + str(request_timestamp))
+    client_socket.settimeout(20) #waits for specific amount of time to receive response before breaking the connection with the Proxy Server and printing Request Time Out Using Except Below
+    response = client_socket.recv(4096).decode() # receives 4096 bytes of data from server(limits data received to 2048 bytes)
+    response_timestamp = datetime.datetime.now()
+    print("At " + str(response_timestamp)+ " received:\n" + str(response))
+    TimeTaken_timestamp = response_timestamp-request_timestamp #delta of first timestamp recorded and last timestamp recorded
+    print("Total Time Taken: "+str(TimeTaken_timestamp.total_seconds()*1000) + " ms") # total round trip time after the client enters the IP address
+    print("Mac Address: "+str(hex(uuid.getnode())))
+except:
+    print("Error: Request Time Out") # if any error print this
---
+++ server_mazenabousaid.py
@@ -0,0 +1,26 @@
+import datetime
+from socket import *
+host = '127.0.0.1' #str(gethostbyname(gethostname())) works on private networks as Firewall Prevents Port Access by default on public networks
+# we can use the localhost IP since both server and client run on same machine
+port = 8696 # port the server binds to
+server_socket = socket(AF_INET, SOCK_STREAM) # instantiate tcp socket
+server_socket.bind((host, port))  # bind host address and port together
+server_socket.listen() # listens for incoming connections
+
+try:
+    connection, address = server_socket.accept() # produces a tuple: in server_socket.accept()[0] we have connection type info, in [1] we have the IP and Port info
+    print("TCP Connection established from " + str(address)+" at " + str(datetime.datetime.now()))
+    ip_request = connection.recv(1024).decode() # Decode IP Address Request Message from Client
+    print("Request from client to connect to " + str(ip_request) + " at " + str(datetime.datetime.now()))
+    dest_socket= socket(AF_INET, SOCK_STREAM) # creates new socket to connect to destination server
+    dest_socket.connect((str(ip_request),80)) # connects to requested destination server via port 80(web)
+    print("Connection to Destination Server established at " + str(datetime.datetime.now()))
+    dest_socket.send(bytes("GET / HTTP/1.1\r\nHost:"+str(ip_request)+":80\r\n\r\n",'utf-8')) # requests page data using GET Method
+    dest_socket.settimeout(20) # waits for specific amount of time to receive response before breaking the connection with the destination server and printing Request Time Out Using Except Below
+    req=dest_socket.recv(4096) # receives back response
+    print("Response received from destination server at "+str(datetime.datetime.now()))
+    connection.send(req) # sends response to client
+    print("Response sent to destination server at " + str(datetime.datetime.now()))
+
+except:
+    print("Error: Request Time Out")# if any error print this
